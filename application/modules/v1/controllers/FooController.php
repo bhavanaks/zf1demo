0164d2b05f5c66a09d7cdaa0b9fdce120ccf41cf
bhavana sathyakeerthi <bhavanasathyakeerthi@gmail.com>
2018-07-06 14:05:06 +0530

    Create FooController.php

---
+++ application/modules/v1/controllers/FooController.php
@@ -0,0 +1,39 @@
+<?php
+class FooController extends Zend_Rest_Controller
+{
+	public function init()
+	{
+		$this->_helper->layout->disableLayout();
+		$this->_helper->viewRenderer->setNoRender(true);
+	}
+	public function indexAction()
+	{
+	  $this->getResponse()->setBody('Hello World');
+	  $this->getResponse()->setHttpResponseCode(200);
+	}
+
+	public function getAction()
+	{
+	  $this->getResponse()->setBody('Foo!');
+	  $this->getResponse()->setHttpResponseCode(200);
+	}
+
+	public function postAction()
+	{
+	  $this->getResponse()->setBody('resource created');
+	  $this->getResponse()->setHttpResponseCode(200);
+	}
+
+	public function putAction()
+	{
+	  $this->getResponse()->setBody('resource updated');
+	  $this->getResponse()->setHttpResponseCode(200);
+	}
+
+	public function deleteAction()
+	{
+	  $this->getResponse()->setBody('resource deleted');
+	  $this->getResponse()->setHttpResponseCode(200);
+	}
+  
+}
